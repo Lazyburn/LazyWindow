7deb2e8527021d86e3cf56a305f37dbc1b255926
LazyBurn <youngnam9@gmail.com>
2017-05-03 23:04:13 +0900

    Initial Commit

---
+++ LazyWindow/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LazyWindow
+//
+//  Created by Youngnam Joun on 5/3/17.
+//  Copyright © 2017 Youngnam Joun. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LazyWindow/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LazyWindow
+//
+//  Created by Youngnam Joun on 5/3/17.
+//  Copyright © 2017 Youngnam Joun. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
